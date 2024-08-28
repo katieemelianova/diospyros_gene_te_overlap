4c91c6ad26ea6dcf9981578143546e7c7232d38a
Katie Emelianova <emelianova@login01.lisc>
2024-08-28 18:08:08 +0200

    dont clean up

---
+++ perform_analysis.sh
@@ -0,0 +1,8 @@
+
+
+
+####################################################################################
+#      symlink the GFAP annotations which have been parsed to be useable by topGO  #
+####################################################################################
+
+ln -s /lisc/scratch/botany/katie/annotation/braker3/*/gfap_annotation/*_topGO_annotation.txt $PWD
